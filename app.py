f75ea6ec30160040d35d0299c0737e82a8e3d506
Stephen Lawrence <stephen@Stephens-MacBook-Pro.local>
2022-07-26 11:10:42 -0600

    changed app IP address

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def update_figs(t,timestamp):
     return combo,spectra,seer.report(spectra),fom,seer.report(fom)
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True,host='0.0.0.0',port='8050')
